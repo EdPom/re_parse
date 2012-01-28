5d67a379e585fa3139f37df34bb187c0a4737960
EdPom <shiveringpen@gmail.com>
2012-01-27 16:18:19 -0800

    add functionality to support NFAtoDFA()

--- main/FA.py
+++ main/FA.py
@@ -2,6 +2,38 @@
 # Description : THis file specifies the data structure of
 #     class FInite Automata.
 
+# acquired from stackoverflow: "How do you remove duplicates from
+# a list in Python whilst preserving order?""
+# I made a mistake here: this funtion is used to remove lists with
+# the first item being the same, so it's not suitable for my use here
+# 
+# def UniqueElem(seq):
+#     seen = set()
+#     seen_add = seen.add
+#     return [ x for x in seq if x not in seen and not seen_add(x)]
+
+# Get this from 'Yet Another Unique() Function' at ActiveState Code
+# http://code.activestate.com/recipes/502263/#c2
+# This works for list of lists
+def UniqueElem(inlist, keepstr=True):
+    typ = type(inlist)
+    if not typ == list:
+        inlist = list(inlist)
+    i = 0
+    while i < len(inlist):
+        try:
+          del inlist[inlist.index(inlist[i], i + 1)]
+        except:
+          i += 1
+    if not typ in (str, unicode):
+        inlist = typ(inlist)
+    else:
+        if keepstr:
+          inlist = ''.join(inlist)
+    return inlist
+
+
+
 def CheckID(ID):
     # States are named with their ID (an integer)
     # Check the given ID is valid
@@ -42,6 +74,12 @@ class State:
         
         # Sucess
         return True
+    
+    def GetTransition(self, key):
+        if key in self.TransitionMap:
+            return self.TransitionMap[key]
+        else:
+            return list()
     
     def Print(self):
         """Used as a debugging tool. This function
@@ -168,10 +206,10 @@ class FiniteAutomata:
 
         # 1 new transition:
         # end of fa1 -> start of fa2
-        self.AddTransition(OldEnd, OldStart2, 'EPSILON')
+        self.AddTransition(OldEnd2, OldStart, 'EPSILON')
 
-        self.StartState.append(OldStart)
-        self.AcceptState.append(OldEnd2)
+        self.StartState.append(OldStart2)
+        self.AcceptState.append(OldEnd)
     
     def CreateAlternation(self, fa2):
         if len(self.StartState) != 1 or len(fa2.StartState) != 1:
@@ -199,10 +237,45 @@ class FiniteAutomata:
         self.AddTransition(OldEnd, NewEnd, 'EPSILON')
         self.AddTransition(OldEnd2, NewEnd, 'EPSILON')
 
-        self.StartState.append(OldStart)
-        self.AcceptState.append(OldEnd2)
+        self.StartState.append(NewStart)
+        self.AcceptState.append(NewEnd)
 
+    def FindEpsilonClosure(self, StateID):
+        
+        EpsilonSet = [StateID,]
+
+        idx = 0
+        while idx < len(EpsilonSet):
+            StatePos = self.FindStatePosByID(EpsilonSet[idx])
+            EpsilonSet.extend(self.States[StatePos].GetTransition('EPSILON'))
+
+            idx += 1
+        
+        return sorted(EpsilonSet[:])
+
+    def FindEpsilonClosureList(self, StateIDs):
+        
+        EpsilonSet = StateIDs
+
+        idx = 0
+        while idx < len(EpsilonSet):
+            StatePos = self.FindStatePosByID(EpsilonSet[idx])
+            EpsilonSet.extend(self.States[StatePos].GetTransition('EPSILON'))
+
+            idx += 1
+        
+        return sorted(EpsilonSet[:])
     
+    def FindTransitionList(self, StateIDs, char):
+        Dest = list()
+        for state in StateIDs:
+            StatePos = self.FindStatePosByID(state)
+            Dest.extend(self.States[StatePos].GetTransition(char))
+        
+        # get this from stackoverflow : "Removing duplicates from list of
+        # lists in Python"
+        return UniqueElem(Dest)
+
     def SetAcceptState(self, ID):
         self.AcceptState.append(ID)
     
@@ -243,10 +316,9 @@ if __name__ == '__main__':
 
     fa = FiniteAutomata(0, 'a')
     fa2 = FiniteAutomata(1, 'b')
+    fa3 = FiniteAutomata(2, 'c')
     fa.CreateAlternation(fa2)
-    # s1 = fa.AddState()
-    # s2 = fa.AddState()
-    # fa.AddTransition(s1, s2, 'a')
-    # fa.SetAcceptState(s2)
-    # fa.SetStartState(s1)
-    fa.Print()
+    fa.CreateAlternation(fa3)
+    print fa.FindEpsilonClosure(fa.GetStartState().pop())
+    # fa.Print()
+
